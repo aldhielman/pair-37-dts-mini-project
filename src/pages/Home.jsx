9065fb02c9a13ce431db483775156f8efa5d50c2
Latif Ariyadi <latifariyadi86@gmail.com>
2022-07-20 14:05:02 +0700

    add footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 };
